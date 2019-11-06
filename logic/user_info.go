a1f394072ba526d537d3a3f6b9c11201b7de1e3c
jiarong.tian <jiarong.tian@hzlt3.com>
2019-11-06 16:27:27 +0800

    sysbench test

--- logic/user_info.go
+++ logic/user_info.go
@@ -1,57 +1,77 @@
 package logic
 
 import (
-    "fmt"
+	"bytes"
+	"fmt"
+	"runtime"
 
-    "ccovers/http_server/global"
-    "ccovers/http_server/lib"
+	"ccovers/http_server/global"
+	"ccovers/http_server/lib"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type GetUserInfoReq struct {
-    UserId int64 `json:"userId" comment:"用户ID"`
+	UserId int64 `json:"userId" comment:"用户ID"`
 }
 
 type GetUserInfoRes struct {
-    UserId  int64  `json:"userId" comment:"用户ID"`
-    Name    string `json:"name" comment:"名称"`
-    Subject string `json:"subject" comment:"科目"`
-    Score   int    `json:"score" comment:"分数"`
+	UserId  int64  `json:"userId" comment:"用户ID"`
+	Name    string `json:"name" comment:"名称"`
+	Subject string `json:"subject" comment:"科目"`
+	Score   int    `json:"score" comment:"分数"`
 }
 
 func GetUserInfo(c *gin.Context) {
-    req := GetUserInfoReq{}
-    res := GetUserInfoRes{}
-    var err error
+	req := GetUserInfoReq{}
+	res := GetUserInfoRes{}
+	var err error
 
-    for ok := true; ok; ok = false {
-        err = lib.ReadRequestBodyJson(c, &req)
-        if err != nil {
-            fmt.Printf("解析错误: %s\n", err.Error())
-            break
-        }
+	for ok := true; ok; ok = false {
+		err = lib.ReadRequestBodyJson(c, &req)
+		if err != nil {
+			fmt.Printf("解析错误: %s\n", err.Error())
+			break
+		}
 
-        err = getUserInfo(&req, &res)
-        if err != nil {
-            fmt.Printf("查询错误: %s\n", err.Error())
-            break
-        }
-        lib.ServerResponse(c, nil, &res)
-        return
-    }
-    lib.ServerResponse(c, err, nil)
+		err = getUserInfo(&req, &res)
+		if err != nil {
+			fmt.Printf("查询错误: %s\n", err.Error())
+			break
+		}
+		lib.ServerResponse(c, nil, &res)
+		return
+	}
+	lib.ServerResponse(c, err, nil)
 }
 
 func getUserInfo(req *GetUserInfoReq, res *GetUserInfoRes) error {
-    sql := fmt.Sprintf(`
-        SELECT id AS user_id, student AS name, subject, score 
+	sql := fmt.Sprintf(`
+        SELECT id AS user_id, student AS name, subject, score
         FROM school_table
         WHERE id = %d;
     `, req.UserId)
-    db := global.GLogic.Raw(sql).Scan(res)
-    if db.Error != nil {
-        return db.Error
-    }
-    return nil
+	db := global.GLogic.Raw(sql).Scan(res)
+	if db.Error != nil {
+		fmt.Println(Stack(3))
+		return db.Error
+	}
+	return nil
+}
+
+func Stack(skip int) string {
+	buf := new(bytes.Buffer)
+
+	callers := make([]uintptr, 32)
+	n := runtime.Callers(skip, callers)
+	frames := runtime.CallersFrames(callers[:n])
+	for {
+		if f, ok := frames.Next(); ok {
+			fmt.Fprintf(buf, "%s\n\t%s:%d (0x%x)\n", f.Function, f.File, f.Line, f.PC)
+		} else {
+			break
+		}
+	}
+	return buf.String()
+
 }
--- main.go
+++ main.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "os"
+	"errors"
+	"fmt"
+	"os"
 
-    "ccovers/http_server/global"
-    "ccovers/http_server/logic"
-    "github.com/gin-gonic/gin"
+	"ccovers/http_server/global"
+	"ccovers/http_server/logic"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    fmt.Println("服务器启动")
+	fmt.Println("服务器启动")
 
-    init_err := global.InitLogicDb()
-    defer global.CloseLogicDb()
-    if init_err != nil {
-        goto EXIT_PROCESS
-    }
-    fmt.Println("logic初始化完成")
+	init_err := global.InitLogicDb()
+	defer global.CloseLogicDb()
+	if init_err != nil {
+		goto EXIT_PROCESS
+	}
+	fmt.Println("logic初始化完成")
 
-    startServer()
+	startServer()
 
 EXIT_PROCESS:
-    fmt.Println("初始化异常，退出程序")
-    fmt.Println(init_err.Error())
-    os.Exit(-1)
+	fmt.Println("初始化异常，退出程序")
+	fmt.Println(init_err.Error())
+	os.Exit(-1)
 }
 
 func startServer() {
-    // gin.SetMode(gin.ReleaseMode)
+	// gin.SetMode(gin.ReleaseMode)
 
-    // 注册一个默认的路由器
-    engine := gin.Default()
-    registRouter(engine)
-    engine.Run(":8080")
+	// 注册一个默认的路由器
+	engine := gin.Default()
+	registRouter(engine)
+	engine.Run(":8080")
 }
 
 func registRouter(engine *gin.Engine) error {
-    if engine == nil {
-        return errors.New("无效的GIN实例")
-    }
+	if engine == nil {
+		return errors.New("无效的GIN实例")
+	}
 
-    router := engine.Group("/v1")
-    {
-        router.POST("/get_user_info", logic.GetUserInfo)
-        router.GET("/get_user_info_ex", logic.GetUserInfoEx)
-    }
-    return nil
+	router := engine.Group("/v1")
+	{
+		router.POST("/get_user_info", logic.GetUserInfo)
+		router.GET("/get_user_info_ex", logic.GetUserInfoEx)
+	}
+	return nil
 }
